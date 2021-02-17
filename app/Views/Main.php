6bae2725a96b6935954dbc0387f0d7c831ca568e
LeandroRD <leandrorduarte@outlook.com>
2021-02-17 20:10:24 -0300

    baixado ficheiros CSS JS bootstrap FontAwesome

--- app/Views/Main.php
+++ app/Views/Main.php
@@ -1,5 +1,7 @@
 <?php
 	//Menu Inicial
 ?>
+<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css')?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css')?>">
 
-<h3>Projeto Geral</h3>
+<h3><i class="fa fa-user mr-3"></i> Projeto Geral</h3>
