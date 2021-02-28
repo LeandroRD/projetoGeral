a63ce8d3e0865a15052db1ce727b161e6a0c4b16
LeandroRD <leandrorduarte@outlook.com>
2021-02-28 09:16:30 -0300

    feito acabamento no admin_new_user

--- app/Views/users/admin_new_user.php
+++ app/Views/users/admin_new_user.php
@@ -1,39 +1,73 @@
 <?php 
     $this->extend('Layout/layout_users');
     $s = session();
+    
 ?>
 
 <?php $this->section('conteudo')?>
-    <!-- erro -->
-    <?php if(isset($error)):?>
-        <div class="alert alert-danger">
-            <?php echo $error ?>
-        </div>
-    <?php endif;?>
-     <!-- formulario para novo usuario -->
-    <h3>Adicionar novo usuario: </h3>
-    <form action="<?php echo site_url('users/admin_new_user') ?>" method="post">
-        <p><input type="text" name="text_username" required placeholder="Username"></p>
-        <p><input type="text" name="text_password" required placeholder="Password"></p>
-        <p><input type="text" name="text_password_repetir" required placeholder="Repetir Password"></p>
-        <div class="form-inline mb-2">
-            <button type="button" class="btn btn-primary btn-sm" id="btn-password">Gerar password</button>
-            <button type="button" class="btn btn-secondary btn-sm" id="btn-limpar">Limpar</button>
-        </div>
-        <p><input type="text" name="text_name"required placeholder="Nome"></p>
-        <p><input type="email" name="text_email" reuired placeholder="Email"></p>
-        <!-- profile -->
-        <p>Profile</p>
-        <label><input type="checkbox" name="check_admin" > Admin</label></br>
-        <label><input type="checkbox" name="check_moderator" > Moderator</label></br>
-        <label><input type="checkbox" name="check_user" checked > User</label></br>
-        
-        <div class="mt-2">
-            <a href="<?php echo site_url('users/admin_users') ?>"class="btn btn-secondary">Cancelar</a>
-            <button class="btn btn-primary">Adicionar</button>
-        </div>    
-    </form>
 
+    <div class="row mt-3 mb-3">
+        <div class="col-6 offset-3">
+            <?php if(isset($error)):?>
+                <div class="alert alert-danger">
+                    <?php echo $error ?>
+                </div>
+            <?php endif;?>
+             <!-- formulario para novo usuario -->
+            <h4 class="text-center">Adicionar novo usuario: </h4>
+            <form action="<?php echo site_url('users/admin_new_user') ?>" method="post">
+                <div class="mb-2">
+                    <input type="text" name="text_username "class="form-control" required placeholder="Username">
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col-6">
+                        <input type="text" name="text_password"class="form-control" required placeholder="Password">
+                    </div>
+                    <div class="col-6 mb-2 text-center">
+                        <button type="button" class="btn btn-primary btn-150 " id="btn-password">Gerar password</button>
+                    </div>
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col-6">
+                        <input type="text" name="text_password_repetir" class="form-control" required placeholder="Repetir Password">      
+                    </div>
+                    <div class="col-6 mb-2 text-center">
+                        <button type="button" class="btn btn-secondary btn-150" id="btn-limpar">Limpar</button>
+                    </div>
+                </div>
+
+                <div class="mb-2">
+                    <input type="text" class="form-control" name="text_name"required placeholder="Nome">
+                </div>
+                <div class="mb-2">
+                    <input type="email" name="text_email" class="form-control" reuired placeholder="Email">   
+                </div>
+
+                
+               
+                
+                <!-- profile -->
+                <h5>Profile</h5>
+                <div class="mb-2 ">
+                    <label class="form-check-label ms-3"><input type="checkbox" class="form-check-input" name="check_admin" > Admin</label></br>
+                    <label class="form-check-label ms-3"><input type="checkbox" class="form-check-input" name="check_moderator" > Moderator</label></br>
+                    <label class="form-check-label ms-3"><input type="checkbox" class="form-check-input" name="check_user" checked > User</label></br>
+                </div>
+                
+                <div class="text-center">
+                    <a href="<?php echo site_url('users/admin_users') ?>"class="btn btn-secondary btn-150">Cancelar</a>
+                    <button class="btn btn-primary btn-150">Adicionar</button>
+                </div>    
+            </form>
+        </div>               
+    </div>
+
+
+    <!-- erro -->
+    
+    
 
 
 
