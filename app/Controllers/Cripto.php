3c1e14e6b99b5c2b001feafa8619bdb374960f1d
LeandroRD <leandrorduarte@outlook.com>
2021-03-01 22:34:29 -0300

    codigo para desincriptar pelo id e  numero_cartao

--- app/Controllers/Cripto.php
+++ app/Controllers/Cripto.php
@@ -5,14 +5,33 @@ use CodeIgniter\Controller;
 
 
 class Cripto extends BaseController{
+    //========================================================
     public function index(){
         echo "estou no cripto";
     }
+    //========================================================
     public function guardarCartao(){
-        $numero_cartao = '764859fd';
+        $numero_cartao = 'Guilherme';
 
         $model = new CriptoModel();
         $model->encriptar($numero_cartao);
         echo 'cartao adicionado com sucesso';
+    }
+    //========================================================
+    public function apresentarCartao($id){
+        $model = new CriptoModel();
+        $resultado = $model->desencriptar($id);
+        echo "<pre>";
+        print_r($resultado);
+        echo "<pre>";    }
+    //========================================================
+    public function procurarCartao($numero_cartao){
+        $model = new CriptoModel();
+        $resultado = $model->procurarCartao($numero_cartao);
+        echo "<pre>";
+        print_r($resultado);
+        echo "<pre>";
+
+
     }
 }
--- app/Models/CriptoModel.php
+++ app/Models/CriptoModel.php
@@ -27,10 +27,39 @@ class CriptoModel extends Model
             ")"
             , $params);
 
+            // exemplo do endereco para incriptar dados na BD
+            // http://localhost/projetogeral/public/index.php/cripto/guardarCartao
+
     }
     //========================================================
     public function desencriptar($id){
-        //retorna desencriptado o cartao de credito   
+        //retorna desencriptado o cartao de credito
+        $params = array(
+            $id
+        ); 
+        return $this->db->query("
+                SELECT id_cartao, AES_DECRYPT(numero_cartao,UNHEX(SHA2('".$this->key."',512))) AS numero_cartao
+                FROM criptografia
+                WHERE id_cartao = ?
+        ",$params)->getResult('array');
+        
+        // exemplo do endereco para procurar cartao pelo id
+        // http://localhost/projetogeral/public/index.php/cripto/apresentarCartao/3
+    }
+    //========================================================
+    public function ProcurarCartao($numero_cartao){
+        $params = array(
+            $numero_cartao
+        );
+        return $this->db->query(
+        "SELECT id_cartao,
+                AES_DECRYPT(numero_cartao,UNHEX(SHA2('".$this->key."',512))) AS numero_cartao
+                from criptografia
+                WHERE AES_DECRYPT(numero_cartao,UNHEX(SHA2('".$this->key."',512)))= ? "
+        ,$params)->getResult('array');
+
+        //Exemplo do endereco a ser consultado na BD
+        // http://localhost/projetogeral/public/index.php/cripto/procurarCartao/Guilherme
     }
 
 }
