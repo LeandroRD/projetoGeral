3ce259377fc0ffb4f34c4cc508d2ab3bae1b02da
LeandroRD <leandrorduarte@outlook.com>
2021-03-04 20:27:18 -0300

    feito os campos de editar e eliminar (aula 62)

--- app/Views/stocks/familias.php
+++ app/Views/stocks/familias.php
@@ -15,22 +15,26 @@
 			--->
 			<div class="row mb-3">
 				<div class="col-6 align-self-end"><h5>Famílias de produtos: </h5></div>
-				<div class="col-6 text-end"><a href=""class="btn btn-primary">Adicionar familia...</a></div>
+				<div class="col-6 text-end"><a href="<?php echo site_url('stocks/familia_adicionar')?>"class="btn btn-primary">Adicionar familia...</a></div>
 			</div>
 			
-			<table class="table table-stripped">
+			<table class="table table-striped">
 				<thead class="table-dark">
 					<th>ID</th>
-					<th>Familia</th>
-					<th>Ações</th>
+					<th>Família</th>
+					<th class="text-end">Ações</th>
 				</thead>
 				<tbody>
 					<?php foreach($familias as $familia):?>
 						<tr>
 							<td><?php echo $familia['id_familia'] ?></td>
 							<td><?php echo $familia['designacao'] ?></td>
+							<td class="text-end">
+								<a href="<?php echo site_url('stocks/familia_editar/'.$familia['id_familia'])?>">Editar</a>
+								<span class="ms-2 me-2">|</span>
+								<a href="<?php echo site_url('stocks/familia_eliminar/'.$familia['id_familia'])?>">Eliminar</a>
+							</td>
 						</tr>
-
 					<?php endforeach;?>
 
 
